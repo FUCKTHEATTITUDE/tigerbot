b177fff91aad8b02935c9299606c551b818800a9
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-08 16:48:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "5926143600:AAHcqLwRHwIQ1GKFjTlCrxRM75eCyaw8tns"
 API_ID = 14782914
-SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
+SESSION_STRING = "BQDhkcIAsuOS32EqZ09uSdXlvChpOh4leOsbhoOCtsEQ4bw5uS4FO5xUPemkLKfP93NC0hNOeoLU8QB2EcUcqx2fA10ePVSFMDr4wDPrUDHXoYhxyxlFdCMeqMfKGYMfsDJqFl7ebMdWJRdeRi5vv3xEgZWtHPSJkTc6VT9SnArfprvgXtDUB4i_7hGBvSxS54k2B4nziG1KiUy4PrA_jS8sxVZF_fLagB0Y5SBDu1DF6qKvuWFhiUnCnxI1w--1SdvT5P3z2a8qeFgKfCxvdgQ89c1j7eapf0wk_EulsJ1eZqE3dWIJMuA8aeX0IDovR5PPTJpg1mlgDAJeVeuTXvupC65stQAAAAE-8Cl3AA" # Check Readme to generate sessions; need for userbot
 API_HASH = "3aa2fabe1074632cf6e2b01da083a2c6"
 USERBOT_PREFIX = "."
 PHONE_NUMBER = "+916381987933"  # Need for Userbot
@@ -13,7 +13,7 @@ GBAN_LOG_GROUP_ID = -1001657538863
 MESSAGE_DUMP_CHAT = -1001657538863
 WELCOME_DELAY_KICK_SEC = 300
 MONGO_URL = "mongodb+srv://ZAID:ZAID@cluster0.8smkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-ARQ_API_KEY = "JGAGCN-IRAJKY-PTPEXD-FCDGHX-ARQ"
+ARQ_API_KEY = "IDYNEY-SLKMLF-YMTCWU-TTTRIW-ARQ"
 ARQ_API_URL = "https://arq.hamker.in"
 LOG_MENTIONS = True
 RSS_DELAY = 300  # In seconds
